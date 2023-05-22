05f264e6ff7773c9fa7a9c324c58ab0c24390482
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-05-22 05:29:54 +0200

    .

--- Collections/src/exercise2/Functions.java
+++
@@ -1,9 +0,0 @@
-package exercise2;
-
-public class Functions {
-}
-
-/*
-Introduce por teclado, hasta que se introduzca “fin”, una serie de nombres, que se insertarán en una colección,
-de forma que se conserve el orden de inserción y que no puedan repetirse. Al final, la colección se mostrará por pantalla.
-*/
