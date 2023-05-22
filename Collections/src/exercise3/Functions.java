0cda3ab0022b2f9482f01dcc5166351910e4285c
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-05-22 08:17:53 +0200

    .

--- Collections/src/exercise3/Functions.java
+++
@@ -1,8 +0,0 @@
-package exercise3;
-
-public class Functions {
-}
-
-/*
-Repite la actividad 2 de forma que se inserten los nombres manteniendo el orden alfabético.
-*/
--- Collections/src/exercise3/Main.java
+++ Collections/src/exercise3/Main.java
@@ -3,7 +3,20 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
+		String word = "";
+		TreeSet<String> list = new TreeSet<String>();
 		Scanner sc = new Scanner(System.in);
+		while(!(word.equalsIgnoreCase("end"))) {
+			System.out.print("\nIntroduce a word: ");
+			word = sc.nextLine();
+			if(!(word.equalsIgnoreCase("end"))) {
+				list.add(word);
+			}
+		}
+		System.out.print("List: ");
+		for(String readWord: list) {
+			System.out.print(" " + readWord);
+		}
 		sc.close();
 	}
 }
